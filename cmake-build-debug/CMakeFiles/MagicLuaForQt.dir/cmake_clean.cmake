faa52ac69941ddfbef36660f90ab3ef29c49a341
sammiler <964518191@qq.com>
2019-05-20 22:45:44 +0800

    提交c++静态反射框架rttr

---
+++ cmake-build-debug/CMakeFiles/MagicLuaForQt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MagicLuaForQt.dir/main.cpp.o"
+  "MagicLuaForQt.pdb"
+  "MagicLuaForQt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MagicLuaForQt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
